e254078d1c0fe1344e38a7b4c6c20fde59c18326
efi2023 <eflevi@g.jct.ac.il>
2024-06-23 03:34:01 +0300

    first methods

--- Ex5Clustering/BitArray.java
+++ Ex5Clustering/BitArray.java
@@ -1,28 +1,46 @@
+import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Set;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class BitArray implements Clusterable<BitArray>{
 	private ArrayList<Boolean> bits;
 
 	public BitArray(String str){
 		// TODO: Complete
+		this.bits = str.chars()
+				.mapToObj(c -> c==1)
+				.collect(Collectors.toCollection(ArrayList::new));
 	}
 	public BitArray(boolean[] bits) {
 		// TODO: Complete
+		this.bits = IntStream.range(0, bits.length)
+				.mapToObj(i -> bits[i])
+				.collect(Collectors.toCollection(ArrayList::new));
 	}
 
 	@Override
 	public double distance(BitArray other) {
 		// TODO: Complete
-		return 0;
+		if (this.bits.size() != other.bits.size()) {return Double.POSITIVE_INFINITY;}
+		int dist = (int) IntStream.range(0, this.bits.size())
+				.filter(i -> !this.bits.get(i).equals(other.bits.get(i))).count();
+		return dist;
 	}
 
 	public static Set<BitArray> readClusterableSet(String path) throws IOException {
 		// TODO: Complete. If the file contains bitarrays of different lengths,
 		//  retain only those of maximal length
-		return null;
+		Stream<String> bitLines = Files.lines(Paths.get(path));
+		Set<BitArray> bitArrays = bitLines
+				.map(BitArray::new)
+				.collect(Collectors.toSet());
+		return bitArrays;
 	}
 
 	@Override
--- Ex5Clustering/TwoDPoint.java
+++ Ex5Clustering/TwoDPoint.java
@@ -1,25 +1,40 @@
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.lang.Math;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TwoDPoint implements Clusterable<TwoDPoint>{
 	double x;
 	double y;
 	public TwoDPoint(String str){
 		// TODO: Complete
+		String[] split = str.split(",");
+		this.x = Double.parseDouble(split[0]);
+		this.y = Double.parseDouble(split[1]);
 	}
 	public TwoDPoint(double x, double y) {
 		// TODO: Complete
+		this.x = x;
+		this.y = y;
 	}
 	@Override
 	public double distance(TwoDPoint other) {
 		// TODO: Complete
-		return 0;
+		return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
 	}
 
 	public static Set<TwoDPoint> readClusterableSet(String path) throws IOException{
 		// TODO: Complete
-		return null;
+		Stream<String> stream = Files.lines(Paths.get(path));
+		Set<TwoDPoint> points = stream
+				.map(TwoDPoint::new)
+				.collect(Collectors.toSet());
+		return points;
 	}
 
 	@Override
